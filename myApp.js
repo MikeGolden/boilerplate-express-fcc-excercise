7a1140fbd35c74a25bf09da93a9f316a3af68e76
Mike Goldenberg <csrutil@tuta.io>
2023-08-22 16:37:57 +0200

    using envvars

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
@@ -19,9 +21,13 @@ app.get("/", function(req, res) {
 
 app.get("/json", (req, res) => {
     res.json({
-      message: "Hello json"
+        if (process.env.MESSAGE_STYLE === "uppercase") {
+            response = "Hello World".toUpperCase();
+          } else {
+            response = "Hello World";
+          }
     });
-})
+});
 
 
 
